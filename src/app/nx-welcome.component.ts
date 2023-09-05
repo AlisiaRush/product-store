3393601d8c938ac2cd07962a651004d68c780841
Alisia Rush <alisiarush@gmail.com>
2023-09-05 12:07:59 -0400

    project setup

--- src/app/nx-welcome.component.ts
+++ src/app/nx-welcome.component.ts
@@ -423,7 +423,7 @@ import { CommonModule } from '@angular/common';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome myngapp 👋
+            Welcome product-crud 👋
           </h1>
         </div>
         <!--  HERO  -->
